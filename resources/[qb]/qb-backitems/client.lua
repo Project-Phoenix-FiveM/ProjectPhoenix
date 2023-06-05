f29e7fb21bd9497dbe96afcfc6bcdd5675dfcf90
Rhodinium <127902502+Rhodiniium@users.noreply.github.com>
2023-06-05 11:27:57 +0100

    Added new backitem script.

---
+++ resources/[qb]/qb-backitems/client.lua
@@ -0,0 +1,231 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+local CurrentBackItems = {}
+local TempBackItems = {}
+local checking = true
+local currentWeapon = nil
+local slots = 40
+local s = {}
+
+
+AddEventHandler('onResourceStart', function(resourceName)
+	if (GetCurrentResourceName() ~= resourceName) then return end
+    BackLoop()
+end)
+
+AddEventHandler('onResourceStop', function(resourceName)
+	if (GetCurrentResourceName() ~= resourceName) then return end
+    resetItems()
+end)
+
+RegisterNetEvent("QBCore:Client:OnPlayerUnload", function()
+    resetItems()
+end)
+
+RegisterNetEvent("backitems:start", function()
+    Wait(10000)
+    BackLoop()
+end)
+
+RegisterNetEvent("backitems:showagain", function()
+    print("[Backitems]: Showing Again")
+    resetItems()
+    check()
+    Wait(10)
+    BackLoop()
+end)
+
+local isInVehicle = false
+local isEnteringVehicle = false
+local currentVehicle = 0
+local currentSeat = 0
+
+CreateThread(function()
+	while true do
+		Wait(0)
+
+		local ped = PlayerPedId()
+
+		if not isInVehicle and not IsPlayerDead(PlayerId()) then
+			if DoesEntityExist(GetVehiclePedIsTryingToEnter(ped)) and not isEnteringVehicle then
+				-- trying to enter a vehicle!
+				local vehicle = GetVehiclePedIsTryingToEnter(ped)
+				local seat = GetSeatPedIsTryingToEnter(ped)
+				local netId = VehToNet(vehicle)
+				isEnteringVehicle = true
+				TriggerEvent('baseevents:enteringVehicle-backitems', vehicle, seat, GetDisplayNameFromVehicleModel(GetEntityModel(vehicle)), netId)
+			elseif not DoesEntityExist(GetVehiclePedIsTryingToEnter(ped)) and not IsPedInAnyVehicle(ped, true) and isEnteringVehicle then
+				-- vehicle entering aborted
+				TriggerEvent('baseevents:enteringAborted-backitems')
+				isEnteringVehicle = false
+			elseif IsPedInAnyVehicle(ped, false) then
+				-- suddenly appeared in a vehicle, possible teleport
+				isEnteringVehicle = false
+				isInVehicle = true
+				currentVehicle = GetVehiclePedIsUsing(ped)
+				currentSeat = GetPedVehicleSeat(ped)
+				local model = GetEntityModel(currentVehicle)
+				local name = GetDisplayNameFromVehicleModel()
+				local netId = VehToNet(currentVehicle)
+				TriggerEvent('baseevents:enteredVehicle-backitems', currentVehicle, currentSeat, GetDisplayNameFromVehicleModel(GetEntityModel(currentVehicle)), netId)
+			end
+		elseif isInVehicle then
+			if not IsPedInAnyVehicle(ped, false) or IsPlayerDead(PlayerId()) then
+				-- bye, vehicle
+				local model = GetEntityModel(currentVehicle)
+				local name = GetDisplayNameFromVehicleModel()
+				local netId = VehToNet(currentVehicle)
+                print("NONCE")
+				TriggerEvent('baseevents:leftVehicle-backitems', currentVehicle, currentSeat, GetDisplayNameFromVehicleModel(GetEntityModel(currentVehicle)), netId)
+				isInVehicle = false
+				currentVehicle = 0
+				currentSeat = 0
+			end
+		end
+		Wait(50)
+	end
+end)
+
+function GetPedVehicleSeat(ped)
+    local vehicle = GetVehiclePedIsIn(ped, false)
+    for i=-2,GetVehicleMaxNumberOfPassengers(vehicle) do
+        if(GetPedInVehicleSeat(vehicle, i) == ped) then return i end
+    end
+    return -2
+end
+
+
+AddEventHandler('baseevents:enteredVehicle-backitems', function(pEntity, pSeat, pName, pClass, pModel)
+    print("remove spin spin car rollercoaster")
+    for k, v in pairs(GetGamePool('CObject')) do
+        if IsEntityAttachedToEntity(PlayerPedId(), v) then
+            SetEntityAsMissionEntity(v, true, true)
+            DeleteObject(v)
+            DeleteEntity(v)
+        end
+    end
+end)
+
+AddEventHandler('baseevents:leftVehicle-backitems', function(pEntity, pSeat, pName, pClass, pModel)
+    print("Okay no more spin spin")
+    TriggerEvent("backitems:showagain")
+end)
+
+
+RegisterNetEvent("backitems:displayItems", function(toggle)
+    if toggle then 
+        for k,v in pairs(TempBackItems) do 
+            createBackItem(k)
+        end
+        BackLoop()
+    else 
+        TempBackItems = CurrentBackItems
+        checking = false
+        for k,v in pairs(CurrentBackItems) do
+            removeBackItem(k)
+        end
+        CurrentBackItems = {}
+    end
+end)
+
+function resetItems()
+    removeAllBackItems()
+    CurrentBackItems = {}
+    TempBackItems = {}
+    currentWeapon = nil
+    s = {}
+    checking = false
+end
+
+function BackLoop()
+    print("[Backitems]: Starting Loop")
+    checking = true
+    CreateThread(function()
+        while checking do
+            local player = QBCore.Functions.GetPlayerData()
+            while player == nil do 
+                player = QBCore.Functions.GetPlayerData()
+                Wait(500)
+            end
+            for i = 1, slots do
+                s[i] = player.items[i]
+            end
+            check()
+            Wait(1000)
+        end
+    end)
+end
+
+function check()
+    for i = 1, slots do
+        if s[i] ~= nil then
+            local name = s[i].name
+            if BackItems[name] then
+                if name ~= currentWeapon then
+                    createBackItem(name)
+                end
+            end
+        end
+    end
+
+    for k,v in pairs(CurrentBackItems) do 
+        local hasItem = false
+        for j = 1, slots do
+            if s[j] ~= nil then
+                local name = s[j].name
+                if name == k then 
+                    hasItem = true
+                end
+            end
+        end
+        if not hasItem then 
+            removeBackItem(k)
+        end
+    end
+end
+
+
+function createBackItem(item)
+    if not CurrentBackItems[item] then
+        if BackItems[item] then 
+            local i = BackItems[item]
+            local model = i["model"]
+            local ped = PlayerPedId()
+            local bone = GetPedBoneIndex(ped, i["back_bone"])
+            RequestModel(model)
+            while not HasModelLoaded(model) do
+                Wait(10)
+            end
+            SetModelAsNoLongerNeeded(model)
+            CurrentBackItems[item] = CreateObject(GetHashKey(model), 1.0, 1.0, 1.0, true, true, false)   
+            AttachEntityToEntity(CurrentBackItems[item], ped, bone, i["x"], i["y"], i["z"], i["x_rotation"], i["y_rotation"], i["z_rotation"], false, false, false, false, 2, true)
+        end
+    end
+end
+
+function removeBackItem(item)
+    if CurrentBackItems[item] then
+        DeleteEntity(CurrentBackItems[item])
+        CurrentBackItems[item] = nil
+    end
+end
+
+function removeAllBackItems()
+    for k,v in pairs(CurrentBackItems) do 
+        removeBackItem(k)
+    end
+end
+
+RegisterNetEvent('weapons:client:SetCurrentWeapon', function(weap, shootbool)
+    if weap == nil then
+        createBackItem(currentWeapon)
+        currentWeapon = nil
+    else
+        if currentWeapon ~= nil then  
+            createBackItem(currentWeapon)
+            currentWeapon = nil
+        end
+        currentWeapon = tostring(weap.name)
+        removeBackItem(currentWeapon)
+    end
+end)
---
+++ resources/[qb]/qb-backitems/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+lua54 'yes'
+
+escrow_ignore {
+}
+
+
+client_scripts {
+    'client.lua',
+    'BackItems.lua',
+}
