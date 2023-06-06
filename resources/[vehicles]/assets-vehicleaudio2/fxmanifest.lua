80c0b66d8f5b28fa2c55556006fe6916511ecb89
Rhodinium <127902502+Rhodiniium@users.noreply.github.com>
2023-06-06 13:40:00 +0100

    cars 2

---
+++ resources/[vehicles]/assets-vehicleaudio2/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+files {
+  'audioconfig/*.dat10',
+  'audioconfig/*.dat10.nametable',
+  'audioconfig/*.dat10.rel',
+  'audioconfig/*.dat151',
+  'audioconfig/*.dat151.nametable',
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54',
+  'audioconfig/*.dat54.nametable',
+  'audioconfig/*.dat54.rel',
+  'sfx/**/*.awc',
+  
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/tamustanggt50_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/tamustanggt50_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_tamustanggt50'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/tascmustanggt50_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/tascmustanggt50_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_tascmustanggt50'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ta4b11_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta4b11_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta4b11'
