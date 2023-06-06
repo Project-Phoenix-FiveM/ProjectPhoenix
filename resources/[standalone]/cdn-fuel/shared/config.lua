8a2dcad1bdc711a35d8e08d31647b394fa1a126f
Rhodinium <127902502+Rhodiniium@users.noreply.github.com>
2023-06-06 20:27:47 +0100

    Config change for fuel script.

--- resources/[standalone]/cdn-fuel/shared/config.lua
+++ resources/[standalone]/cdn-fuel/shared/config.lua
@@ -103,7 +103,7 @@ Config.RefuelAnimationDictionary = "timetable@gardener@filling_can" -- This is f
 --- Player Owned Gas (Gasoline) Ergonomic Refueling Stations (Poggers) ---
 Config.PlayerOwnedGasStationsEnabled = true -- When true, peds will be located at all gas stations, and players will be able to talk with peds & purchase gas stations, having to manage fuel supplies.
 Config.StationFuelSalePercentage = 0.65 -- % of sales that the station gets. If they sell 4 Liters of Gas for $16 (not including taxes), they will get 16*Config.StationFuelSalePercentage back from the sale. Treat this as tax, also, it balances the profit margins a bit.
-Config.EmergencyShutOff = false -- When true, players can walk up to the ped and shut off the pumps at a gas station. While false, this option is disabled, because it can obviously be an issue. 
+Config.EmergencyShutOff = true -- When true, players can walk up to the ped and shut off the pumps at a gas station. While false, this option is disabled, because it can obviously be an issue. 
 Config.UnlimitedFuel = false -- When true, the fuel stations will not require refuelling by gas station owners, this is for the early stages of implementation.
 Config.MaxFuelReserves = 100000 -- This is the maximum amount that the fuel station's reserves can hold.
 Config.FuelReservesPrice = 2.0 -- This is the price of fuel reserves for gas station owners.
