cf228ebf3f10300a16562d98a02bc968f9bd78b0
Rhodinium <127902502+Rhodiniium@users.noreply.github.com>
2023-06-06 13:39:24 +0100

    overlaps removed.

--- resources/[qb]/qb-vehicles/fxmanifest.lua
+++ resources/[qb]/qb-vehicles/fxmanifest.lua
@@ -1,26 +1,11 @@
 fx_version 'cerulean'
 game 'gta5'
-description 'QB-VehicleKeys'
+description 'QB'
 version '1.2.5'
 
 
-data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
-data_file 'HANDLING_FILE' 'handling.meta'
-data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
-data_file 'CARCOLS_FILE' 'carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
-
-files {
-    'vehiclelayouts.meta',
-    'handling.meta',
-    'vehicles.meta',
-    'carcols.meta',
-    'carvariations.meta',
-  }
-
 client_script {
     'client/*.lua',
-    'vehicle_names.lua'
 }
 
 
