61c19b19bc5eeece575115705a0f3c45ebaa2d28
Rhodinium <127902502+Rhodiniium@users.noreply.github.com>
2023-06-07 16:59:40 +0100

    Overhauled CalmAI Script.

--- resources/[qb]/qb-smallresources/client/calmai.lua
+++ resources/[qb]/qb-smallresources/client/calmai.lua
@@ -1,23 +1,144 @@
---  Relationship Types:
---  0 = Companion
---  1 = Respect
---  2 = Like
---  3 = Neutral
---  4 = Dislike
---  5 = Hate
+local curWatchingPeds = {}
+local relationshipTypes = {
+  "PLAYER",
+  "COP",
+  "MISSION2",
+  "MISSION3",
+  "MISSION4",
+  "MISSION5",
+  "MISSION6",
+  "MISSION7",
+  "MISSION8",
+}
 
-SetRelationshipBetweenGroups(1, `AMBIENT_GANG_HILLBILLY`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `AMBIENT_GANG_BALLAS`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `AMBIENT_GANG_MEXICAN`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `AMBIENT_GANG_FAMILY`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `AMBIENT_GANG_MARABUNTE`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `AMBIENT_GANG_SALVA`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `AMBIENT_GANG_LOST`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `GANG_1`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `GANG_2`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `GANG_9`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `GANG_10`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `FIREMAN`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `MEDIC`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `COP`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `PRISONER`, `PLAYER`)
+
+Citizen.CreateThread(function()
+  while true do
+  Wait(600)
+      for _, group in ipairs(relationshipTypes) do
+        if group == "COP" then
+          SetRelationshipBetweenGroups(3, `PLAYER`,GetHashKey(group))
+          SetRelationshipBetweenGroups(3, GetHashKey(group), `PLAYER`)
+          SetRelationshipBetweenGroups(0, `MISSION2`,GetHashKey(group))
+          SetRelationshipBetweenGroups(0, GetHashKey(group), `MISSION2`)
+
+        else
+          SetRelationshipBetweenGroups(0, `PLAYER`,GetHashKey(group))
+          SetRelationshipBetweenGroups(0, GetHashKey(group), `PLAYER`)
+          SetRelationshipBetweenGroups(0, `MISSION2`,GetHashKey(group))
+          SetRelationshipBetweenGroups(0, GetHashKey(group), `MISSION2`)
+        end  
+      SetRelationshipBetweenGroups(5, GetHashKey(group), `MISSION8`)
+    end
+
+
+    SetRelationshipBetweenGroups(1, `PLAYER`, `AMBIENT_GANG_WEICHENG`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_WEICHENG`, `PLAYER`)
+    SetRelationshipBetweenGroups(1, `PLAYER`, `AMBIENT_GANG_FAMILY`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_FAMILY`, `PLAYER`)
+    SetRelationshipBetweenGroups(1, `PLAYER`, `AMBIENT_GANG_BALLAS`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_BALLAS`, `PLAYER`)
+
+    SetRelationshipBetweenGroups(1, `PLAYER`, `AMBIENT_GANG_SALVA`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_SALVA`, `PLAYER`)
+    SetRelationshipBetweenGroups(1, `PLAYER`, `AMBIENT_GANG_MEXICAN`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_MEXICAN`, `PLAYER`)
+
+
+
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_LOST`, `AMBIENT_GANG_WEICHENG`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_WEICHENG`, `AMBIENT_GANG_LOST`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_LOST`, `AMBIENT_GANG_FAMILY`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_FAMILY`, `AMBIENT_GANG_LOST`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_LOST`, `AMBIENT_GANG_BALLAS`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_BALLAS`, `AMBIENT_GANG_LOST`)
+
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_LOST`, `AMBIENT_GANG_SALVA`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_SALVA`, `AMBIENT_GANG_LOST`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_LOST`, `AMBIENT_GANG_MEXICAN`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_MEXICAN`, `AMBIENT_GANG_LOST`)
+
+
+
+--WEST SIDE
+    SetRelationshipBetweenGroups(1, `MISSION4`, `AMBIENT_GANG_WEICHENG`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_WEICHENG`, `MISSION4`)
+
+-- MEDIC / POLICE WEST SIDE
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_WEICHENG`, `MISSION2`)
+    SetRelationshipBetweenGroups(1, `MISSION2`, `AMBIENT_GANG_WEICHENG`)
+
+
+
+--CENTRAL
+    SetRelationshipBetweenGroups(1, `MISSION5`, `AMBIENT_GANG_FAMILY`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_FAMILY`, `MISSION5`)
+    SetRelationshipBetweenGroups(1, `MISSION5`, `AMBIENT_GANG_BALLAS`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_BALLAS`, `MISSION5`)
+
+-- MEDIC / POLICE CENTRAL
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_BALLAS`, `MISSION2`)
+    SetRelationshipBetweenGroups(1, `MISSION2`, `AMBIENT_GANG_BALLAS`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_FAMILY`, `MISSION2`)
+    SetRelationshipBetweenGroups(1, `MISSION2`, `AMBIENT_GANG_FAMILY`)
+
+
+
+
+
+
+--EAST SIDE
+    SetRelationshipBetweenGroups(1, `MISSION6`, `AMBIENT_GANG_SALVA`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_SALVA`, `MISSION6`)
+    SetRelationshipBetweenGroups(1, `MISSION6`, `AMBIENT_GANG_MEXICAN`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_MEXICAN`, `MISSION6`)
+
+-- MEDIC / POLICE EAST SIDE
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_SALVA`, `MISSION2`)
+    SetRelationshipBetweenGroups(1, `MISSION2`, `AMBIENT_GANG_SALVA`)
+    SetRelationshipBetweenGroups(1, `MISSION2`, `AMBIENT_GANG_MEXICAN`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_MEXICAN`, `MISSION2`)
+
+
+
+
+
+    SetRelationshipBetweenGroups(1, -86095805, `MISSION2`)
+    SetRelationshipBetweenGroups(1, `MISSION2`, -86095805)
+
+    SetRelationshipBetweenGroups(1,1191392768, `MISSION2`)
+    SetRelationshipBetweenGroups(1, `MISSION2`,1191392768)
+    
+    SetRelationshipBetweenGroups(1, `MISSION2`, 45677184)
+    SetRelationshipBetweenGroups(1, 45677184, `MISSION2`)
+
+
+
+
+    SetRelationshipBetweenGroups(3, `PLAYER`, `MISSION7`)
+    SetRelationshipBetweenGroups(3, `MISSION7`, `PLAYER`)
+
+    SetRelationshipBetweenGroups(0, `MISSION7`, `COP`)
+    SetRelationshipBetweenGroups(0, `COP`, `MISSION7`)
+
+    SetRelationshipBetweenGroups(0, `MISSION2`, `MISSION7`)
+    SetRelationshipBetweenGroups(0, `MISSION7`, `MISSION2`)
+
+    SetRelationshipBetweenGroups(0, `MISSION7`, `MISSION7`)
+
+    SetRelationshipBetweenGroups(3, `COP`,`PLAYER`)
+    SetRelationshipBetweenGroups(3, `PLAYER`, `COP`)
+
+    SetRelationshipBetweenGroups(0, `PLAYER`, `MISSION3`)
+    SetRelationshipBetweenGroups(0, `MISSION3`,`PLAYER`)
+
+    -- LOST MC
+    SetRelationshipBetweenGroups(1, `PLAYER`, `AMBIENT_GANG_LOST`)
+    SetRelationshipBetweenGroups(1, `AMBIENT_GANG_LOST`, `PLAYER`)
+    SetRelationshipBetweenGroups(1,  `COP`, `AMBIENT_GANG_LOST`)
+    SetRelationshipBetweenGroups(1,  `AMBIENT_GANG_LOST`, `COP`)
+
+  end
+end)
+
+
