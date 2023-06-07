6d79c2f61a8c11975c5ebd1adb82a0cfa271e352
Rhodinium <127902502+Rhodiniium@users.noreply.github.com>
2023-06-08 00:48:05 +0100

    gabz 6

---
+++ resources/[gabz]/cfx-gabz-ottos/fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Gabz'
+description 'Ottos'
+version '1.0.0'
+lua54 'yes'
+this_is_a_map 'yes'
+
+dependencies { 
+    '/server:4960',     -- ⚠️PLEASE READ⚠️; Requires at least SERVER build 4960.
+    
+    'cfx-gabz-mapdata', -- ⚠️PLEASE READ⚠️; Requires [cfx-gabz-mapdata] to work properly.
+    'cfx-gabz-247', -- ⚠️PLEASE READ⚠️; Requires [cfx-gabz-247] to work properly.
+    'cfx-gabz-pizzeria', -- ⚠️PLEASE READ⚠️; Requires [cfx-gabz-pizzeria] to work properly.
+}
+
+server_scripts {
+    'version_check.lua',
+}
+
+escrow_ignore {
+    'stream/**/*.ytd',
+}
+dependency '/assetpacks'
---
+++ resources/[gabz]/cfx-gabz-pdm/fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Gabz'
+description 'PDM'
+version '1.0.0'
+lua54 'yes'
+this_is_a_map 'yes'
+
+dependencies { 
+    '/server:4960',     -- ⚠️PLEASE READ⚠️; Requires at least SERVER build 4960.
+    
+    'cfx-gabz-mapdata', -- ⚠️PLEASE READ⚠️; Requires [cfx-gabz-mapdata] to work properly.
+}
+
+server_scripts {
+    'version_check.lua',
+}
+
+escrow_ignore {
+    'stream/**/*.ytd',
+    'stream/ydr/gabz_pdm_flag1.yft',
+    'stream/ydr/gabz_pdm_flag2.yft',
+}
+dependency '/assetpacks'
