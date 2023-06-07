02bb544a1d26a9c5da86185ca867f453014a8d21
Gascanu <gascanumystery@gmail.com>
2023-06-07 20:57:00 +0300

    Renewed Update (Simplified Code)

--- resources/[standalone]/Renewed-Banking/client/framework.lua
+++ resources/[standalone]/Renewed-Banking/client/framework.lua
@@ -1,14 +1,5 @@
-local Framework = Config.framework
-QBCore, ESX, FullyLoaded = nil, nil, nil
-if Framework == 'qb'then
-	QBCore = exports['qb-core']:GetCoreObject()
-    FullyLoaded = Framework == 'qb' and LocalPlayer.state.isLoggedIn
-elseif Framework == 'esx'then
-    ESX = exports['es_extended']:getSharedObject()
-    FullyLoaded = Framework == 'esx' and ESX.PlayerLoaded or false
-else
-	print('^6[^3Renewed-Banking^6]^0 Unsupported Framework detected!')
-end
+QBCore = exports['qb-core']:GetCoreObject()
+FullyLoaded = QBCore and LocalPlayer.state.isLoggedIn
 
 AddStateBagChangeHandler('isLoggedIn', nil, function(_, _, value)
     FullyLoaded = value
@@ -28,12 +19,6 @@ AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
     initalizeBanking()
 end)
 
-RegisterNetEvent('esx:playerLoaded', function(xPlayer)
-    Wait(100)
-    FullyLoaded = true
-    initalizeBanking()
-end)
-
 AddEventHandler('onResourceStart', function(resourceName)
     Wait(100)
     if resourceName ~= GetCurrentResourceName() then return end
@@ -43,9 +28,5 @@ end)
 
 
 AddEventHandler('QBCore:Client:OnPlayerUnload', function()
-    DeletePeds()
-end)
-
-AddEventHandler('esx:onPlayerLogout', function()
     DeletePeds()
 end)
