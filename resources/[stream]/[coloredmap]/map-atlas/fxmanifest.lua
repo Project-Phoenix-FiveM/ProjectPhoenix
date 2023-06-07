f1970f0ccf4da307cefc48f46d46bcdaa08e8c08
Gascanu <gascanumystery@gmail.com>
2023-06-07 21:20:34 +0300

    cayo map combined

---
+++ resources/[stream]/[coloredmap]/map-atlas/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version('bodacious')
+game('gta5')
+
+data_file "SCALEFORM_DLC_FILE" "stream/int3232302352.gfx" -- Minimap Cayo
+
+files {
+  "stream/int3232302352.gfx"
+}
+client_script "client.lua"
--- resources/[stream]/[coloredmap]/minimap_cayo/fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-fx_version('bodacious')
-game('gta5')
-
-data_file "SCALEFORM_DLC_FILE" "stream/int3232302352.gfx"
-
-files {
-  "stream/int3232302352.gfx"
-}
