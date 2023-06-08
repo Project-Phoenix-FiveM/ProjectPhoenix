194cdde9eb517f574cca8c3ee2c9fd6024493a98
Rhodinium <127902502+Rhodiniium@users.noreply.github.com>
2023-06-08 22:34:41 +0100

    Removed Hospital MLO Overlap.

--- resources/[defaultmaps]/hospital_map/ipl.lua
+++
@@ -1,7 +0,0 @@
-Citizen.CreateThread(function()
-  RequestIpl("rc12b_default")
-  RemoveIpl("rc12b_default")
-  RequestIpl("rc12b_destroyed")
-  RemoveIpl("rc12b_destroyed")
-  RequestIpl("rc12b_hospitalinterior")
-end)
