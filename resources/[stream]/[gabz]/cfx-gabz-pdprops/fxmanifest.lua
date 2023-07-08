ecb31c1aa09baf0fc0cea240ddf1e64d9fb8a72e
Rhodinium <127902502+Rhodiniium@users.noreply.github.com>
2023-07-08 17:07:06 +0100

    m

---
+++ resources/[stream]/[gabz]/cfx-gabz-pdprops/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Gabz'
+description 'PD Props'
+version '1.0.0'
+lua54 'yes'
+this_is_a_map 'yes'
+
+dependencies { 
+    '/server:4960',     -- ⚠️PLEASE READ⚠️; Requires at least SERVER build 4960.
+    '/gameBuild:2545',  -- ⚠️PLEASE READ⚠️; Requires at least GAME build 2545.
+}
+
+server_scripts {
+    'version_check.lua',
+}
+
+escrow_ignore {
+    'stream/**/*.ytd',
+}
+dependency '/assetpacks'
