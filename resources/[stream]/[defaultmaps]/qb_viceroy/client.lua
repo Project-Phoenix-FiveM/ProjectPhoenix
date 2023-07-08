fb9cc816d63f0ac522e203d8abf2f2d077f39704
Rhodinium <127902502+Rhodiniium@users.noreply.github.com>
2023-07-08 17:02:25 +0100

    m

---
+++ resources/[stream]/[defaultmaps]/qb_viceroy/client.lua
@@ -0,0 +1,8 @@
+
+Citizen.CreateThread(function()
+   
+    -- Pillbox hospital: 307.1680, -590.807, 43.280
+    PillboxHospital.Enable(true)
+
+end)
+
---
+++ resources/[stream]/[defaultmaps]/qb_viceroy/fxmanifest.lua
@@ -0,0 +1,16 @@
+-- Resources:
+-- **********
+-- IPL list: https://wiki.rage.mp/index.php?title=Interiors_and_Locations
+
+fx_version 'cerulean'
+game 'gta5'
+
+client_scripts {
+	"lib/common.lua"
+	, "lib/observers/interiorIdObserver.lua"
+	, "lib/observers/officeSafeDoorHandler.lua"
+	, "client.lua"
+
+	-- GTA V
+	, "gtav/pillbox_hospital.lua"
+}
