70dcf6bb8296ef784a9cf43bd4e82048029e9fa4
Rhodinium <127902502+Rhodiniium@users.noreply.github.com>
2023-07-08 17:04:50 +0100

    m

---
+++ resources/[stream]/[gabz]/cfx-gabz-impound/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Gabz'
+description 'Impound'
+version '1.0.0'
+lua54 'yes'
+this_is_a_map 'yes'
+
+dependencies { 
+    '/server:4960',     -- ⚠️PLEASE READ⚠️; Requires at least SERVER build 4960.
+    '/gameBuild:2545',  -- ⚠️PLEASE READ⚠️; Requires at least GAME build 2545.
+    'cfx-gabz-mapdata', -- ⚠️PLEASE READ⚠️; Requires [cfx-gabz-mapdata] to work properly.
+}
+
+server_scripts {
+    'version_check.lua',
+}
+
+escrow_ignore {
+    'stream/**/*.ytd',
+}
+dependency '/assetpacks'
