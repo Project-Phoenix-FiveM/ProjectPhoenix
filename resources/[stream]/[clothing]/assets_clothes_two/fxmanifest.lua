09b44c4face297cfcf626d6a342d76cdc8615d55
Rhodinium <127902502+Rhodiniium@users.noreply.github.com>
2023-07-08 16:56:13 +0100

    m

---
+++ resources/[stream]/[clothing]/assets_clothes_two/fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_six.meta',
+  'mp_f_freemode_01_mp_f_six.meta',
+  'stream/pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_six.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_six.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations.meta'
