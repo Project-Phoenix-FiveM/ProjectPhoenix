e267a92ea4ffcffc5085c6f0004149066b9a8ca1
Rhodinium <127902502+Rhodiniium@users.noreply.github.com>
2023-07-08 16:52:56 +0100

    m

---
+++ resources/[stream]/[clothing]/assets_clothes_one/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+  "mp_f_freemode_01_np_shop.meta",
+  "mp_m_freemode_01_np_shop.meta",
+  'mp_m_freemode_01_mp_m_np_new.meta',
+  'mp_f_freemode_01_mp_f_np_new.meta'
+}
+
+data_file "SHOP_PED_APPAREL_META_FILE" "mp_f_freemode_01_np_shop.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "mp_m_freemode_01_np_shop.meta"
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_np_new.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_np_new.meta'
